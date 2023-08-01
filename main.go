6f6b9e6959b5bb331b6fe8c5a5975c48b528e3d8
lukashdias <lukas.dias@grupomateus.com.br>
2023-07-31 22:11:12 -0300

    add markdown

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Product struct {
+	Name string
+	Price float64
+	Quantity int
+	Description string
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 
@@ -14,5 +21,16 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "index.html", nil)
+	produtos := []Product{
+		{"Mouse", 25.00, 10, "Mouse sem fio"},
+		{"Teclado", 50.00, 10, "Teclado sem fio"},
+		{"Monitor", 500.00, 10, "Monitor 4k"},
+		{"Notebook", 2500.00, 10, "Notebook Dell"},
+		{"Impressora", 500.00, 10, "Impressora HP"},
+		{"Cadeira", 250.00, 10, "Cadeira Gamer"},
+		{"Mesa", 500.00, 10, "Mesa Gamer"},
+		{"Gabinete", 250.00, 10, "Gabinete Gamer"},
+		{"Placa de Vídeo", 2500.00, 10, "Placa de Vídeo RTX 2080"},
+	}
+	temp.ExecuteTemplate(w, "index.html", produtos)
 }
